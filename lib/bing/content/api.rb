4f17d39dbd827e76e16a34b63cbc6f3b19d22b2c
Paul David <paul@denknerd.org>
2017-04-20 16:25:09 -0700

    Initial commit, auto-gen gem with Bundler.

---
+++ lib/bing/content/api.rb
@@ -0,0 +1,9 @@
+require "bing/content/api/version"
+
+module Bing
+  module Content
+    module Api
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/bing/content/api/version.rb
@@ -0,0 +1,7 @@
+module Bing
+  module Content
+    module Api
+      VERSION = "0.1.0"
+    end
+  end
+end
