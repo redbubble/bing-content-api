21d1835dc7795bf2b8bb5f3c451dc53c4c5d6d13
Paul David <paul@denknerd.org>
2017-04-24 16:38:41 -0700

    WIP must change token logic.

--- spec/bing/content/api/client_spec.rb
+++ spec/bing/content/api/client_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe Bing::Content::Api::Client do
   context "has valid refresh_token" do
 
     subject(:bing_client) {
-      Bing::Content::Api::Client.new("app_id_foo",
+      Bing::Content::Api::Client.new(
+        "app_id_foo",
         "dev_token_foo",
         "merchant_id_foo",
         refresh_token="foobarbaz") }
@@ -28,5 +29,30 @@ RSpec.describe Bing::Content::Api::Client do
         bing_client.authorise!
       end
     end
+  end
+  context "has invalid refresh_token" do
+
+    subject(:bing_client) {
+      Bing::Content::Api::Client.new(
+        "app_id_foo",
+        "dev_token_foo",
+        "merchant_id_foo",
+        refresh_token="foobarbazINVALID") }
+
+    it "gets the user to intervene and provide OAuth code" do
+      VCR.use_cassette("oauth-invalid-refresh") do
+        expect(bing_client).to receive(:first_time_authorise!) # eq("refreshtokenBBBfoo")
+        bing_client.authorise!
+      end
+    end
+
+    # it "makes callback with new refresh token" do
+    #   VCR.use_cassette("oauth-refresh") do
+    #     callback = lambda { |token| }
+    #     expect(callback).to receive(:call).with("refreshtokenAAAfoo")
+    #     bing_client.refresh_token_callback = callback
+    #     bing_client.authorise!
+    #   end
+    # end
   end
 end
