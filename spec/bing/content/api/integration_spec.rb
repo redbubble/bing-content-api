a2453529eb4ea754010f219b04e0dcb24a25eb53
Paul David <paul@denknerd.org>
2017-04-26 15:30:55 -0700

    Add integration test.

---
+++ spec/bing/content/api/integration_spec.rb
@@ -0,0 +1,38 @@
+RSpec.describe Bing::Content::Api::Client do
+
+  context "has valid refresh_token" do
+    let(:application_id) { "app_id_foo" }
+    let(:developer_token) { "dev_token_foo" }
+    let(:merchant_id) { "123" }
+    let(:auth) { "refresh_token" }
+
+    let(:product) { build(:product) }
+
+    subject(:bing_client) {
+      Bing::Content::Api::Client.new(
+        application_id,
+        developer_token,
+        merchant_id,
+        refresh_token=auth) }
+
+    it "successfully inserts a product into the catalog" do
+      VCR.use_cassette("insert-product-integration") do
+        bing_client.refresh_token!
+
+        batch = Bing::Content::Api::Batch.new
+        batch.add_insertions([product])
+
+        bing_client.runBatch(batch)
+
+        catalogue = bing_client.retrieve_catalogue
+        expected_product = product.to_record
+        expect(catalogue.length).to eq(1)
+        catalogue_product = catalogue[0]
+
+        expected_product.each do |key, val|
+          expect(catalogue_product[key]).to eq(val)
+        end
+      end
+    end
+  end
+end
