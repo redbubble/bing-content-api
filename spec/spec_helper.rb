db82d9f2d26bb55232bd6858fcff25437c5c7ca1
Paul David <paul@denknerd.org>
2017-04-26 14:31:15 -0700

    Put VCR config in global spot.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,8 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :faraday
+end
