488317572c9d8974076739b2f6b95213f7b2207e
Paul David <paul@denknerd.org>
2017-04-27 11:21:14 -0700

    Kill empty module.

--- lib/bing/content/api.rb
+++ lib/bing/content/api.rb
@@ -6,11 +6,3 @@ require "bing/content/api/batch"
 require "bing/content/api/response"
 require "bing/content/api/batch_processor"
 require "bing/content/api/batch_operation"
-
-module Bing
-  module Content
-    module Api
-      # Your code goes here...
-    end
-  end
-end
