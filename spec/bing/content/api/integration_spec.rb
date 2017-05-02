001ed60fc3189cb20a185ceb8e1293a260f04e47
Paul David <paul@denknerd.org>
2017-05-02 13:44:53 -0700

    Be more intentional about which fields we're expecting.

--- spec/bing/content/api/integration_spec.rb
+++ spec/bing/content/api/integration_spec.rb
@@ -6,7 +6,20 @@ RSpec.describe Bing::Content::Api::Client do
     let(:merchant_id) { "123" }
     let(:auth) { "refresh_token" }
 
-    let(:product) { build(:product) }
+    let(:product) { build(:product,
+      offer_id: "3051759-US-sticker",
+      title: "Awesome sticker",
+      description: "this is the best sticker in the world",
+      price: 6.66,
+      currency: "USD",
+      product_link: "https://www.redbubble-staging.com/people/toothbrush/works/3051759-a-thing-a-church?p=sticker&size=small",
+      image_link: "https://ih1.redbubble-staging.net/image.7202580.1759/sticker,375x360-bg,ffffff.jpg",
+      target_country: "US",
+      content_language: "en",
+      availability: "in stock",
+      channel: "online",
+      condition: "new",
+    ) }
 
     subject(:bing_client) {
       Bing::Content::Api::Client.new(
@@ -27,8 +40,8 @@ RSpec.describe Bing::Content::Api::Client do
         expect(catalogue.length).to eq(1)
         catalogue_product = catalogue[0]
 
-        expected_product.each do |key, val|
-          expect(catalogue_product[key]).to eq(val)
+        ["offer_id", "title", "description", "price", "product_link", "image_link", "target_country", "content_language", "availability", "channel", "condition"].each do |key|
+          expect(catalogue_product[key]).to eq(expected_product[key])
         end
       end
     end
