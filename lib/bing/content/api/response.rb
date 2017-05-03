bfdcb7c9c8184e124496e8dd6fd71fb5557157f4
Paul David <paul@denknerd.org>
2017-05-03 15:33:31 -0700

    Properly report on batch errors.

--- lib/bing/content/api/response.rb
+++ lib/bing/content/api/response.rb
@@ -5,18 +5,18 @@ module Bing
         attr_reader :failures, :successes
 
         def initialize(response_entries, batch)
-          @failures = batch.all_products
+          @failures = []
           @successes = []
 
           response_entries.each do |entry|
             id = entry["batchId"].to_i
-            add_success(batch.product_by_batch_id(id))
+            error = entry["errors"]
+            if error
+              failures << { response: entry, product: batch.product_by_batch_id(id) }
+            else
+              successes << { response: entry, product: batch.product_by_batch_id(id) }
+            end
           end
-        end
-
-        def add_success(product)
-          @failures.delete_if { |prod| prod.offer_id == product.offer_id }
-          @successes << product
         end
       end
     end
